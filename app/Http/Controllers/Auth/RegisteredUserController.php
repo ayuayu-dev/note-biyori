deaf149da7351fa69256bee2b333f38911dfb17d
Ayumi Mitani <ayumimitani3@gmail.com>
2026-08-05 18:29:43 +0900

    アカウント新規登録処理／データベース名をnote_biyoriに変更

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -21,6 +21,7 @@ class RegisteredUserController extends Controller
      */
     public function create(): Response
     {
+        //アカウント新規登録画面を表示する
         return Inertia::render('Auth/Register');
     }
 
@@ -31,22 +32,27 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        //入力内容をバリデーションでチェック
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        //usersテーブルへユーザー情報を登録
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            //パスワードはハッシュ化する
             'password' => Hash::make($request->password),
         ]);
 
         event(new Registered($user));
 
+        //ユーザー情報登録処理後は自動ログイン
         Auth::login($user);
 
+        //登録完了後、ホーム画面へ飛ぶ
         return redirect(route('dashboard', absolute: false));
     }
 }
