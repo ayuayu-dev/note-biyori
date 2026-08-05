61db3e3daa3a519788cb00d477cfca16adb8e9bb
Ayumi Mitani <ayumimitani3@gmail.com>
2026-08-05 15:48:19 +0900

    ファビコンをノートびよりのアイコンに変更

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="icon" type="image/png" href="/favicon.png">
+        
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
