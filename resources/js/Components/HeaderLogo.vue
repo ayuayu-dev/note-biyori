d6cbd0dc6c93052e2529cdcda365ca8e476d32ab
Ayumi Mitani <ayumimitani3@gmail.com>
2026-08-17 16:30:05 +0900

    ダッシュボードのロゴ画像を変更

---
+++ resources/js/Components/HeaderLogo.vue
@@ -0,0 +1,12 @@
+<!-- resources/js/Components/HeaderLogo.vue -->
+<script setup lang="ts">
+import headerLogo from '@/images/logo-header.png';
+</script>
+
+<template>
+    <img 
+        :src="headerLogo" 
+        alt="ノートびより" 
+        class="h-10 w-auto object-contain" 
+    />
+</template>
