4b95589dd24fb3c8f73182cbe130d66a9c955efd
Ayumi Mitani <ayumimitani3@gmail.com>
2026-08-31 15:38:06 +0900

    postsとpost_imagesのマイグレーションを追加

---
+++ database/migrations/2026_08_31_061042_create_post_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_images', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('post_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('image_path');
+
+            $table->unsignedInteger('sort_order');
+
+            $table->timestamps();
+
+            $table->unique(['post_id', 'sort_order']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_images');
+    }
+};
