6e23e879db415c06ddeda6f5b86bd2b0833579ae
Ayumi Mitani <ayumimitani3@gmail.com>
2026-09-06 16:30:20 +0900

    投稿画像データの保存処理を追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,10 +12,18 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
+        // アップロードされた画像をStorage（storage\app\public）に保存する
         $path = $request->file('image')->store('posts', 'public');
 
+        // ログイン中のユーザーに紐づく投稿を作成する
         $post = Post::create([
             'user_id' => $request->user()->id,
+        ]);
+
+        // 保存した画像を投稿に紐づけてpost_imagesに登録する
+        $post->images()->create([
+            'image_path' => $path,
+            'sort_order' => 1,
         ]);
     }
 }
--- app/Models/PostImage.php
+++ app/Models/PostImage.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PostImage extends Model
 {
+    /**
+     * 一括代入可能な属性
+     */
+    protected $fillable = [
+        'image_path',
+        'sort_order',
+    ];
+
+    /**
+     * 投稿を取得
+     */
     public function post(): BelongsTo
     {
         return $this->belongsTo(Post::class);
